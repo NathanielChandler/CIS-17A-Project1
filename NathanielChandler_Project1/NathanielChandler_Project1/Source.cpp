c7e493758a6415bfb3dfb294fafbc1c6ea1b53b1
Nathaniel <chandlerocks4@gmail.com>
2017-03-01 00:48:21 -0800

    Main test 3 - progress
    -Current starts at NW corner
    -error with border calc if out of range of vector

--- NathanielChandler_Project1/NathanielChandler_Project1/Source.cpp
+++ NathanielChandler_Project1/NathanielChandler_Project1/Source.cpp
@@ -6,8 +6,16 @@ using namespace std;
 int main()
 {
 	Map map1 = Map(3);
-
+	Player player = Player();
+	char temp;
+	bool notDone = true;
 	cout << to_string(map1.getSize()) << endl;
 	cout << map1.getCurrent().GetName() << endl;
+	while (notDone)
+	{
+		cin >> temp;
+		notDone = map1.goDirection(temp, player.GetLvl());
+		cout << map1.getCurrent().GetName() << endl;
+	}
 	return 0;
 }
